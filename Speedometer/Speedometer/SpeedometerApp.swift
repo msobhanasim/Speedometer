f52d06d0766016493ee68d586e00ab2de662d978
M Sobhan Asim <50422345+msobhanasim@users.noreply.github.com>
2025-08-02 11:26:17 +0400

    project setup

---
+++ Speedometer/Speedometer/SpeedometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedometerApp.swift
+//  Speedometer
+//
+//  Created by Sobhan Asim on 02/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct SpeedometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
